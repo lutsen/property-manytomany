b180bc995a48cd0c0759b3eb3e6d87cd4e296f81
Lútsen Stellingwerff <lutsen@hoverkraft.nl>
2016-05-17 22:07:00 +0100

    First!

---
+++ README.md
@@ -0,0 +1,13 @@
+[<img src="https://cdn.rawgit.com/lutsen/lagan/master/lagan-logo.svg" width="100" alt="Lagan">](https://github.com/lutsen/lagan)
+
+Lagan One-to-many Property Controller
+=====================================
+
+Controller for the Lagan many-to-many property.  
+Lets the user define a many-to-many relation between two content entries.
+
+The Manytomany property type controller enables a many-to-many relation between 2 Lagan entries. The name of the property should be the name of the Lagan model this model can have a many-to-many relation with. For this to work properly the other model should have a many-to-many relation with this model as well. So in our example in the Lagan project the Lagan Hoverkraft model has a many-to-many relation with the Lagan Feature model, and the Lagan Feature model has a many-to-many relation with the Lagan Hoverkraft model.
+
+To be used with [Lagan](https://github.com/lutsen/lagan). Lagan lets you create flexible content objects with a simple class, and manage them with a web interface.
+
+Lagan is a project of [Lútsen Stellingwerff](http://lutsen.land/) from [HoverKraft](http://www.hoverkraft.nl/).
---
+++ src/Manytomany.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Lagan\Property;
+
+/**
+ * Controller for the Lagan many-to-many property.
+ * Lets the user define a many-to-many relation between two content entries.
+ * The Manytomany property type controller enables a many-to-many relation between 2 Lagan models.
+ * The name of the property should be the name of the Lagan model this model can have a many-to-many
+ * relation with. For this to work properly the other model should have a many-to-many relation with this model as well.
+ * So in our example in the Lagan project the Lagan Hoverkraft model has a many-to-many relation
+ * with the Lagan Feature model, and the Lagan Feature model has a many-to-many relation with the Lagan Hoverkraft model.
+ *
+ * A property type controller can contain a set, read, delete and options method. All methods are optional.
+ * To be used with Lagan: https://github.com/lutsen/lagan
+ */
+
+class manytomany {
+
+	/**
+	 * The set method is executed each time a property with this type is set.
+	 *
+	 * @param bean		$bean		The Redbean bean object with the property.
+	 * @param array		$property	Lagan model property arrray.
+	 * @param integer[]	$new_value	An array with id's of the objects the object with this property has a many-to-many relation with.
+	 *
+	 * @return boolean	Returns false because a many-to-many relation is automaticaly stored in a separate database table
+	 */
+	public function set($bean, $property, $new_value) {
+
+		$list = [];
+		foreach ($new_value as $id) {
+			if ($id) {
+				$list[] = \R::load(  $property['name'], $id );
+			}
+		}
+
+		$bean->{ 'shared'.ucfirst($property['name']).'List' } = $list;
+		\R::store($bean);
+
+		return false;
+
+	}
+
+	/**
+	 * The read method is executed each time a property with this type is read.
+	 *
+	 * @param bean		$bean		The Readbean bean object with this property.
+	 * @param string[]	$property	Lagan model property arrray.
+	 *
+	 * @return bean[]	Array with Redbean beans with a many-to-many relation with the entry with this property.
+	 */
+	public function read($bean, $property) {
+
+		return  $bean->{ 'shared'.ucfirst($property['name']).'List' };
+
+	}
+
+	/**
+	 * The options method returns all the optional values this property can have,
+	 * but NOT the ones it currently has.
+	 *
+	 * @param bean		$bean		The Readbean bean object with this property.
+	 * @param array		$property	Lagan model property arrray.
+	 *
+	 * @return bean[]	Array with all beans of the $property['name'] Lagan model.
+	 */
+	public function options($bean, $property) {
+
+		if ( $bean ) {
+
+			// List of beans who allready have a many-to-many ralation with this bean
+			$relations = $bean->{ 'shared'.ucfirst($property['name']).'List' };
+			if ($relations) {
+
+				$relations_ids = [];
+				foreach ($relations as $relation) {
+					$relations_ids[] = $relation->id;
+				}
+
+				return	\R::find( $property['name'],
+						' id NOT IN ('.\R::genSlots( $relations_ids ).') ',
+						$relations_ids );
+			} else {
+				return \R::findAll( $property['name'] );
+			}
+
+		} else {
+
+			return \R::findAll( $property['name'] );
+
+		}
+
+	}
+
+}
+
+?>
